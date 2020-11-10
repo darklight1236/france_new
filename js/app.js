55f2cab73d28d1f7826c573af2a16c7b809e1558
84991 <84991@PC3003>
2020-11-10 19:55:07 +0400

    форма почти готова, не хватает капчи

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@ function pushCheck(id) {
     let check = document.querySelector('#'+ id +'_check');
     check.classList.toggle('check_active');
 }
+
+function labelShow(id){
+    let input = document.querySelector(`#${id}`).value; 
+    let el = document.querySelector(`#d_${id}`);
+    if(input.trim().length > 0){
+        el.style.display = "block";
+    } else {
+        el.style.display = "none";
+    }    
+}

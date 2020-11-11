9a14bcbf02e3893242378a83ddc05127a9fce609
антон викторович денисов <83037@tyazhmash.com>
2020-11-11 18:41:08 +0400

    немного адаптива на 1 стр и сделанные иконки звука

--- js/audio.js
+++ js/audio.js
@@ -1,28 +1,45 @@
 class CustomAudio{
-    constructor(controls){
-        console.log(controls);
+    constructor(element){
 
-        this.controls = controls;
-        let block = controls.closest('.person_block');
-        block.querySelector('.loader').style.display = 'none';
-        this.controls.addEventListener("click", function() {
-            
-            if( this.controls.className == "controls play"){
-                this.controls.classList.remove('play');
-                this.controls.classList.add('stop');
-                block.querySelector('.loader').style.display = 'flex';
+        this.element = element;
+        let controls = this.element;
+        console.log(controls);
+        let loader = controls.parentNode.querySelector('.loader');
+        controls.addEventListener("click", function() {
+
+            let AllAudio = document.querySelectorAll(".controls");
+            AllAudio.forEach(item => {
+                if( item != controls ){
+                    item.classList.remove("stop");
+                    item.classList.add("play");
+                }
+            });
+            let AllLoaders = document.querySelectorAll(".loader");
+            AllLoaders.forEach(item => {
+                if( item != loader ){
+                    item.style.opacity = '0';
+                }
+            });
+
+
+
+            if (controls.className == "controls play" ){
+                controls.classList.remove("play");
+                controls.classList.add("stop");
+                loader.style.opacity = "1";
             }else{
-                this.controls.classList.remove('stop');
-                this.controls.classList.add('play');
-                block.querySelector('.loader').style.display = 'none';
+                controls.classList.remove("stop");
+                controls.classList.add("play");
+                loader.style.opacity = "0";
             }
         })
+
     }
 }
 
 
 window.addEventListener("load",function(){
-    document.querySelectorAll(".play").forEach(element => {
+    document.querySelectorAll(".controls").forEach(element => {
         new CustomAudio(element)
     });
 });
